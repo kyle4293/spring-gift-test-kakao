14f0bfda6c0ee67d5e5340ed74aa633ab8568fcf
boorownie <probitanima11@gmail.com>
2026-02-22 19:05:31 +0900

    Refactor REST controllers to use annotations for request mapping and update package structure for acceptance tests

--- src/main/java/gift/ui/CategoryRestController.java
+++ src/main/java/gift/ui/CategoryRestController.java
@@ -3,10 +3,7 @@ package gift.ui;
 import gift.application.CategoryService;
 import gift.application.CreateCategoryRequest;
 import gift.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +12,7 @@ import java.util.List;
 public class CategoryRestController {
     private final CategoryService categoryService;
 
-    public CategoryRestController(final CategoryService categoryService) {
+    public CategoryRestController(@RequestBody final CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
